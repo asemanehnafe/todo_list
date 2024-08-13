3ae8206bfa97e1e49aa05c87feef77c45dfe5ae5
asemanehnafe <asemanehnafe@gmail.com>
2024-08-13 15:58:48 +0330

    refactor(task): add priority choices

--- todo_list/models/task.py
+++ todo_list/models/task.py
@@ -2,10 +2,21 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    deadline = models.DateField()
-    priority = models.IntegerField()
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+    PRIORITY_CHOICES = [
+        (HIGH, "High"),
+        (MEDIUM, "Medium"),
+        (LOW, "Low"),
+    ]
+
+    title = models.CharField(max_length=100, verbose_name="عنوان")
+    description = models.TextField(verbose_name="توضیحات")
+    deadline = models.DateField(verbose_name="تاریخ سررسید")
+    priority = models.IntegerField(
+        choices=PRIORITY_CHOICES, default=MEDIUM, verbose_name="الویت"
+    )
 
     class Meta:
         ordering = ["deadline", "priority"]
